c617bf4c8ee02ab47c7aa97f300ee51f0a6a4cb9
Daniel Borowski <daniel.borowski@coderspro.pl>
2022-11-15 16:23:01 +0100

    Initial commit

---
+++ src/main/java/com/example/javafxdemo/GridpaneController.java
@@ -0,0 +1,16 @@
+package com.example.javafxdemo;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class GridpaneController {
+    @FXML
+    private Button btnOne;
+    @FXML
+    private Button btnTwo;
+    @FXML
+    private Button btnThree;
+    @FXML
+    private Button btnFour;
+}
